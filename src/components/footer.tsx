fc9abb159c45a9e2765a91dece2f513b49bc6961
tiaraclrssa <Suciclarissatiara@gmail.com>
2024-12-01 02:10:07 +0700

    Authentication

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -58,6 +58,10 @@ export default function Footer() {
             d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
         </svg>
       </Link>
+      <li className="text-white bg-black font-bold">
+  <Link href="/SignUpPage">Sign Up</Link>
+      </li>
+
     </div>
   </nav>
 </footer>
