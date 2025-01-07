eca06b35a1e3d72bb8dc31ca4f70a35b014092c6
nuaysa <66248753+nuaysa@users.noreply.github.com>
2025-01-07 10:07:15 +0700

    Update dateFormatDB.tsx

--- src/helpers/dateFormatDB.tsx
+++ src/helpers/dateFormatDB.tsx
@@ -1,14 +1,9 @@
-const formatDatetimeForDB = (date: string, time: string): string => {
-  const datetime = new Date(`${date}T${time}Z`);
+const formatDatetimeForDB  = (date: string): string => {
+  const datetime = new Date(date);
 
   const year = datetime.getUTCFullYear();
   const month = String(datetime.getUTCMonth() + 1).padStart(2, "0"); 
   const day = String(datetime.getUTCDate()).padStart(2, "0");
-  const hours = String(datetime.getUTCHours()).padStart(2, "0");
-  const minutes = String(datetime.getUTCMinutes()).padStart(2, "0");
-  const seconds = String(datetime.getUTCSeconds()).padStart(2, "0");
 
-  const formattedDate = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.000Z`;
-
-  return formattedDate;
+  return `${year}-${month}-${day}`;
 };
