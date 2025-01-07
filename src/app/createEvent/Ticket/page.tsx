3e83fb60c38d692ebbb6f9a943d174b3aae6be51
nuaysa <66248753+nuaysa@users.noreply.github.com>
2025-01-07 09:44:14 +0700

    Update page.tsx

--- src/app/createEvent/Ticket/page.tsx
+++ src/app/createEvent/Ticket/page.tsx
@@ -165,7 +165,7 @@ export default function CreateTicket() {
             
             <Field
               name="discount"
-              type="radio"
+              type="checkout"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2"
               onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                 const value = e.target.value;
